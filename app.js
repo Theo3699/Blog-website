149d3e7ad853de0a7415b5708f079e52495f3f0e
Theo <samson.theodor3699@gmail.com>
2022-09-15 13:58:41 +0300

    Finish project

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -10,13 +11,22 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+var diaryInfos = [];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-  res.render("home", {htmlHome: "Home", htmlParagraph: homeStartingContent});
+  res.render("home", {
+    htmlHome: "Home", htmlParagraph: homeStartingContent,
+    allInfo: diaryInfos
+  });
+})
+
+app.get("/home", function(req, res){
+  res.redirect("/");
 })
 
 app.get("/about", function(req, res){
@@ -32,10 +42,26 @@ app.get("/compose", function(req, res){
 })
 
 app.post("/compose", function(req, res){
-  console.log("Entered post/compose");
+  var newDiaryTitle = req.body.newDiaryTitle;
+  var newDiaryEntry = req.body.newDiaryEntry;
+  var post = {title: newDiaryTitle, body: newDiaryEntry};
+  diaryInfos.push(post);
+  res.redirect("/");
 })
 
-
+app.get("/posts/:pathParam", function(req, res){
+  var requestTitle = req.params.pathParam;
+  var ok = false;
+  diaryInfos.forEach(function(post){
+    if (_.lowerCase(requestTitle) === _.lowerCase(post.title)){
+      ok = true;
+      res.render("post", {allInfo: diaryInfos, ejsTitle: post.title});
+    }
+  });
+  if (!ok) {
+    res.send("Bad request");
+  }
+})
 
 
 
